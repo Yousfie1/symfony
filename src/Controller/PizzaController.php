07cf43927a54bc0728e9eb2b215a3c99f51e3620
SD Student <302444804@student.rocmondriaan.nl>
2022-03-29 14:22:23 +0200

    push

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +14,20 @@ class PizzaController extends  AbstractController
      */
     public function homepage()
     {
-        $cats=['vis','kaas','vlees','drank'];
+        $cats=['vis','vega','vlees','fiets','dag'];
 
         return $this->render('pizza/pizza.html.twig',['cats'=>$cats]);
     }
+
+    /**
+     * @Route("/category")
+     */
+    public function category(EntityManagerInterface $em)
+    {
+        $cats=$em->getRepository(Category::class)->findAll();
+        //dd($cats);
+
+        return $this->render('pizza/category.html.twig',['cats'=>$cats]);
+    }
+
 }
